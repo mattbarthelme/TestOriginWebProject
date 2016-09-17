d85d787a60409590eb7ef86840da1169f3074e9a
mattbarthelme <matt.barthelme@transamerica.com>
2016-09-17 11:19:51 -0500

    Initial checkin
    Initial checkin

---
+++ src/com/barthelme/test/Hello.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.barthelme.test;
+
+/**
+ * @author matt
+ *
+ */
+public class Hello {
+	
+	private String hello;
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+	
+	public String printHello() {
+		
+		return "Hello there!";
+	}
+	
+
+}
